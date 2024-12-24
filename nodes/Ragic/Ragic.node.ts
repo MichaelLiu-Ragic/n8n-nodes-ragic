e7bbef6d530a8cf5129ab12cf79207351c66590a
MICHAELLIU\tw010 <michaelliu@ragic.com>
2024-12-24 10:39:03 +0800

    將form的選項改為都顯示 新增刷新功能 新增record選項，但先不給他功能

--- nodes/Ragic/Ragic.node.ts
+++ nodes/Ragic/Ragic.node.ts
@@ -46,44 +46,82 @@ export class Ragic implements INodeType {
       type: 'options',
       typeOptions: {
         loadOptionsMethod: 'getFormOptions',
+        loadOptionsDependsOn: ['credentials'],
       },
       default: '',
       description: 'Only the forms that you are the admin user would show in this list.',
     },
-		]
+    {
+      displayName: 'Record',
+      name: 'record',
+      type: 'options',
+      displayOptions: {
+        show: {
+          action: [
+            'updateExistedData',
+          ],
+        },
+	    },
+      typeOptions: {
+        loadOptionsMethod: 'getRecordOptions',
+        loadOptionsDependsOn: ['form'],
+      },
+      default: ''
+    }
+    ]
 	};
 
   methods = {
     loadOptions: {
       async getFormOptions(this: ILoadOptionsFunctions) {
-        // 獲取當前選擇的 `action` 值
-        const action = this.getNodeParameter('action') as string;
-  
-        // 如果選擇的是 `createNewData`，發送 API 請求
-        if (action === 'createNewData') {
-          const credentials = await this.getCredentials('RagicApi');
-          const serverName = credentials?.serverName as string;
-          const apiKey = credentials?.apiKey as string;
-          const responseString = await this.helpers.request({
-            method: 'GET',
-            url: `https://${serverName}?api&n8n`,
-            headers: {
-              Authorization: `Basic ${apiKey}`,
-            },
-          });
+        const credentials = await this.getCredentials('RagicApi');
+        const serverName = credentials?.serverName as string;
+        const apiKey = credentials?.apiKey as string;
+        const responseString = await this.helpers.request({
+          method: 'GET',
+          url: `https://${serverName}?api&n8n`,
+          headers: {
+            Authorization: `Basic ${apiKey}`,
+          },
+        });
 
-          const responseArray = JSON.parse(responseString);
-  
-          // 假設回傳的 JSON 結構為 [{ id: '1', name: 'Form 1' }, { id: '2', name: 'Form 2' }]
-          return responseArray.map((form: { displayName: string; path: string }) => ({
-            name: form.displayName,
-            value: form.path,
-          }));
-        }
-  
-        // 如果選擇的是其他動作，返回空選項
-        return [];
+        const responseArray = JSON.parse(responseString);
+
+        // 假設回傳的 JSON 結構為 [{ id: '1', name: 'Form 1' }, { id: '2', name: 'Form 2' }]
+        return responseArray.map((form: { displayName: string; path: string }) => ({
+          name: form.displayName,
+          value: form.path,
+        }));
       },
+      async getRecordOptions(this: ILoadOptionsFunctions) {
+        // // 獲取當前選擇的 `action` 值
+        // const action = this.getNodeParameter('action') as string;
+  
+        // // 如果選擇的是 `updateExistedData`，發送 API 請求
+        // if (action === 'updateExistedData') {
+        //   const credentials = await this.getCredentials('RagicApi');
+        //   const serverName = credentials?.serverName as string;
+        //   const apiKey = credentials?.apiKey as string;
+        //   const responseString = await this.helpers.request({
+        //     method: 'GET',
+        //     url: `https://${serverName}?api&n8n`,
+        //     headers: {
+        //       Authorization: `Basic ${apiKey}`,
+        //     },
+        //   });
+
+        //   const responseArray = JSON.parse(responseString);
+  
+        //   // 假設回傳的 JSON 結構為 [{ id: '1', name: 'Form 1' }, { id: '2', name: 'Form 2' }]
+        //   return responseArray.map((form: { displayName: string; path: string }) => ({
+        //     name: form.displayName,
+        //     value: form.path,
+        //   }));
+        // }
+  
+        // // 如果選擇的是其他動作，返回空選項
+        return [];
+      }
     },
 
 
